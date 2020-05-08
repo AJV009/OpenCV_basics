6ac44b7a3f9d7a8468cf1faadf7669d5725789d2
Alphons Jaimon <jaimonalphons@gmail.com>
2020-05-09 01:22:24 +0530

    cv

--- preprocessing.py
+++ preprocessing.py
@@ -43,9 +43,37 @@ cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPL
 cnts = imutils.grab_contours(cnts)
 output = image.copy()
 # loop over the contours
-for c in cnts:
+# for c in cnts:
 	# draw each contour on the output image with a 3px thick purple
 	# outline, then display the output contours one at a time
-	cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
-	cv2.imshow("Contours", output)
-	cv2.waitKey(0)
+# 	cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
+# 	cv2.imshow("Contours", output)
+# 	cv2.waitKey(0)
+
+# text = "I found {} objects!".format(len(cnts))
+# cv2.putText(output, text, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (240, 0, 159), 2)
+# cv2.imshow("Contours", output)
+# cv2.waitKey(0)
+
+# we apply erosions to reduce the size of foreground objects
+mask = thresh.copy()
+mask = cv2.erode(mask, None, iterations=5)
+# reduce the contour sizes with 5 iterations
+# cv2.imshow("Eroded", mask)
+# cv2.waitKey(0)
+
+# similarly, dilations can increase the size of the ground objects
+mask = thresh.copy()
+mask = cv2.dilate(mask, None, iterations=5)
+# cv2.imshow("Dilated", mask)
+# cv2.waitKey(0)
+
+# a typical operation we may want to apply is to take our mask and
+# apply a bitwise AND to our input image, keeping only the masked
+# regions
+mask = thresh.copy()
+output = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
+
+
